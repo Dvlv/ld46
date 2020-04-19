cb97f1e8dca24d87a4bfcfa23bab1940290f21c5
Dvlv <Dvlv@users.noreply.github.com>
2020-04-19 07:14:31 +0100

    player anim

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,15 +15,32 @@ onready var sprite = $AnimatedSprite
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		position.y -= move_speed * delta
+
+		sprite.rotation_degrees = 0
+		sprite.play("walk")
+
 	elif Input.is_action_pressed("ui_down"):
 		position.y += move_speed * delta
+		sprite.rotation_degrees = 180
+		sprite.play("walk")
 
 	if Input.is_action_pressed("ui_left"):
 		position.x -= move_speed * delta
+		sprite.rotation_degrees = 270
+		sprite.play("walk")
+
 	elif Input.is_action_pressed("ui_right"):
 		position.x += move_speed * delta
+		sprite.rotation_degrees = 90
+		sprite.play("walk")
+
+	var is_acting = false
+
 
 	if Input.is_action_just_pressed("player_action"):
+		is_acting = true
+		sprite.play("action")
+
 		if near_sq:
 			emit_signal("player_action_near_sq")
 		elif is_near_apples and not has_apple:
@@ -31,8 +48,17 @@ func _physics_process(delta):
 			has_apple = true
 
 	if Input.is_action_just_pressed("player_apple") and has_apple:
+		is_acting = true
+		sprite.play("action")
 		emit_signal("give_apple", near_sq)
 		has_apple = false
+
+	if not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_down") \
+	and not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_right") \
+	and (Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right")
+	or Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_down")):
+		sprite.play("idle")
+
 
 
 
